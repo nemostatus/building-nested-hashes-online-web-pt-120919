5b7cc133c1eaebe7925724843ccce159fbf45c63
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-19 06:44:04 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,8 +20,8 @@ def bonus
    }
   }
 
-  #code your solution here:
-
+  epic_tragedy hero status
+epic_tragedy heroine status
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
