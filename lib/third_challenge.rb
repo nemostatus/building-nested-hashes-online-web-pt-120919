c2aee56777bae7510fc7e14b19d4afa1e7095dc8
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-19 06:27:04 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         name:"Lord Montague",
         age: "53"
         },
-      :matriarch => {
+      :matriarch => {name:"Lady Montague"
         
         },
       :hero => {
