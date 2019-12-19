ce9e857c9706af46f574cf8761ed9314682a1a86
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-19 06:48:47 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,7 +20,7 @@ def bonus
    }
   }
 
-  epic_tragedy[:Montague][:hero][:status] = "dead"
+  epic_tragedy[:Montague][:hero][:status]= "dead"
 epic_tragedy[:Capulet][:heroine][:status]="dead"
   
 
